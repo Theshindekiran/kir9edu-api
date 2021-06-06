f764f5cd90d48408d01e9625278d8f0ba5e8b276
sameer <sasukemestry@gmail.com>
2021-06-06 19:33:18 +0530

    last commit

--- app/models/index.js
+++ app/models/index.js
@@ -4,9 +4,10 @@ const dbConfig = require("../config/db.config.js");
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-const db = {};
+const db = {}; 
 db.mongoose = mongoose;
 db.url = dbConfig.url;
 db.users = require("./user.model.js")(mongoose);
+//db.products = require("./product.model.js")(mongoose);
 
 module.exports = db;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: ["http://localhost:8081", "http://localhost:4200", "https://kira9edu.herokuapp.com/", "https://kira9edu.herokuapp.com"]
+  origin: ["https://kira9edu.herokuapp.com/", "https://kira9edu.herokuapp.com", "http://kira9edu.herokuapp.com", "http://localhost:8081", "http://localhost:4200", ]
 };
 
 app.use(cors(corsOptions));
@@ -38,8 +38,24 @@ app.get("/", (req, res) => {
 
 require("./app/routes/user.route")(app);
 
+//websocket 
+const WebSocket = require('ws')
+ 
+const wss = new WebSocket.Server({ port: 8040 })
+ 
+wss.on('connection', ws => {
+  onConnection(ws);
+  ws.on('message', message => {
+    //onmessage(message, ws);
+    console.log(`Received message => ${message}`)
+  })
+  ws.send('Hello! Message From Server!!')
+})
+
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8050;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+
